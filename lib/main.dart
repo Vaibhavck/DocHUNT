2e495e34463ba5ef4e7127c7c15b781c72b93077
Vaibhav Kanade <eparis@atlassian.com>
2020-06-08 21:32:02 +0530

    Basic Flutter App

--- lib/main.dart
+++ lib/main.dart
@@ -134,6 +134,7 @@ class _HomeState extends State<Home> {
                             fontSize: 24.0,
                             fontWeight: FontWeight.bold,
                           ),
+                          
                         ),
                         Switch(
                           inactiveTrackColor: Colors.grey,
